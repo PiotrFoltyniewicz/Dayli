f927b60161ee27cd446c34db7e1019fe0c40f085
Piotr Foltyniewicz <piotr.foltyniewicz@gmail.com>
2023-09-27 14:46:04 +0200

    Update HabitModel.cs

--- Models/HabitModel.cs
+++ Models/HabitModel.cs
@@ -2,5 +2,17 @@
 {
     public class HabitModel
     {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public bool Status { get; set; }
+
+        public HabitModel(int id, DateTime date, string title, bool status)
+        {
+            Id = id;
+            Date = date;
+            Title = title;
+            Status = status;
+        }
     }
 }
