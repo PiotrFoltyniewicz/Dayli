9bec3022f7b81f1c1801610237904b9f1da01c19
Piotr Foltyniewicz <piotr.foltyniewicz@gmail.com>
2023-10-06 14:07:51 +0200

    Implemented HabitListController and Model

--- Controllers/HabitListController.cs
+++ Controllers/HabitListController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using BetterDay.Models;
+using Newtonsoft.Json;
 
 namespace BetterDay.Controllers
 {
@@ -9,22 +11,27 @@ namespace BetterDay.Controllers
     [Authorize]
     public class HabitListController : ControllerBase
     {
-        [HttpGet("list")]
-        public async Task<IActionResult> GetHabitsList()
+        [HttpGet]
+        public async Task<IEnumerable<HabitListModel>> GetHabitsList()
         {
-            return null;
+            string currUser = TokenHandler.GetCurrentUser(User.Claims);
+            return await HabitListModel.GetHabitList(currUser);
         }
 
         [HttpPut("add/{title}")]
         public async Task<IActionResult> AddHabitToList(string title)
         {
-            return null;
+            string currUser = TokenHandler.GetCurrentUser(User.Claims);
+            var result = await HabitListModel.AddHabitToList(currUser, title);
+            return new JsonResult(result);
         }
 
         [HttpDelete("delete/{id}")]
         public async Task<IActionResult> DeleteHabitFromList(int id)
         {
-            return null;
+            string currUser = TokenHandler.GetCurrentUser(User.Claims);
+            var result = await HabitListModel.DeleteHabitFromList(currUser, id);
+            return new JsonResult(result);
         }
     }
 }
--- Models/HabitListModel.cs
+++ Models/HabitListModel.cs
@@ -1,4 +1,7 @@
-﻿namespace BetterDay.Models
+﻿using MySqlConnector;
+using BetterDay.Errors;
+
+namespace BetterDay.Models
 {
     public class HabitListModel
     {
@@ -9,6 +12,70 @@
         {
             Id = id;
             Title = title;
+        }
+        public async static Task<IEnumerable<HabitListModel>> GetHabitList(string username)
+        {
+            var connection = new MySqlConnection("Server=localhost;User ID=root;Password=;Database=betterdaydb");
+            await connection.OpenAsync();
+
+            var query = new MySqlCommand(@$"SELECT h.Id, h.Title
+                                            FROM habitslist AS h
+                                            INNER JOIN users AS u
+                                            ON h.UserID = u.ID
+                                            WHERE u.Username = '{username}';", connection);
+            var reader = await query.ExecuteReaderAsync();
+
+            List<HabitListModel> habitlist = new List<HabitListModel>();
+            while (await reader.ReadAsync())
+            {
+                habitlist.Add(new HabitListModel((int)reader[0], (string)reader[1]));
+            }
+            await reader.CloseAsync();
+            await connection.CloseAsync();
+            return habitlist;
+        }
+        public async static Task<ApiError> AddHabitToList(string username, string title)
+        {
+            ApiError response;
+            var connection = new MySqlConnection("Server=localhost;User ID=root;Password=;Database=betterdaydb");
+            await connection.OpenAsync();
+
+            var query = new MySqlCommand(@$"INSERT INTO habitslist (UserID, Title)
+                                            VALUES ({TokenHandler.GetUserId(username).Result},
+                                                    '{title}');", connection);
+            var reader = await query.ExecuteReaderAsync();
+            if (reader.RecordsAffected == 0)
+            {
+                response = new ApiError(444, "Habit not added");
+            }
+            else
+            {
+                response = new ApiError(200, "Habit successfully added");
+            }
+            await reader.CloseAsync();
+            await connection.CloseAsync();
+            return response;
+        }
+        public async static Task<ApiError> DeleteHabitFromList(string username, int id)
+        {
+            ApiError response;
+            var connection = new MySqlConnection("Server=localhost;User ID=root;Password=;Database=betterdaydb");
+            await connection.OpenAsync();
+
+            var query = new MySqlCommand(@$"DELETE FROM habitslist
+                                            WHERE ID = {id} AND UserId = {TokenHandler.GetUserId(username).Result};", connection);
+            var reader = await query.ExecuteReaderAsync();
+            if (reader.RecordsAffected == 0)
+            {
+                response = new ApiError(444, "Habit not deleted");
+            }
+            else
+            {
+                response = new ApiError(200, "Habit successfully deleted");
+            }
+            await reader.CloseAsync();
+            await connection.CloseAsync();
+            return response;
         }
     }
 }
