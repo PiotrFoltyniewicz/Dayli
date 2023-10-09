7a6d8952cc3da7627bb766a031e7b0c31f3e68f9
Piotr Foltyniewicz <piotr.foltyniewicz@gmail.com>
2023-10-09 16:48:20 +0200

    blud

---
+++ Models/HabitGroupModel.cs
@@ -0,0 +1,22 @@
+﻿namespace BetterDay.Models
+{
+    public class HabitGroupModel
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        private List<HabitModel> habits;
+
+        public HabitGroupModel(int id, DateTime date, IEnumerable<HabitModel> habits) 
+        {
+            Id = id;
+            Date = date;
+            this.habits = habits.ToList();
+        }
+
+        public HabitModel this[int i]
+        {
+            get { return habits[i]; }
+            set { habits[i] = value; }
+        }
+    }
+}
--- Models/HabitModel.cs
+++ Models/HabitModel.cs
@@ -1,18 +1,42 @@
-﻿namespace BetterDay.Models
+﻿using MySqlConnector;
+
+namespace BetterDay.Models
 {
     public class HabitModel
     {
         public int Id { get; set; }
-        public DateTime Date { get; set; }
         public string Title { get; set; }
         public bool Status { get; set; }
 
-        public HabitModel(int id, DateTime date, string title, bool status)
+        public HabitModel(int id, string title, bool status)
         {
             Id = id;
             Date = date;
             Title = title;
             Status = status;
+        }
+
+        public async static Task<IEnumerable<HabitModel>> GetAllUserHabits(string username)
+        {
+            var connection = new MySqlConnection("Server=localhost;User ID=root;Password=;Database=betterdaydb");
+            await connection.OpenAsync();
+
+            var query = new MySqlCommand(@$"SELECT n.Id, n.Date, n.Note
+                                            FROM notes AS n
+                                            INNER JOIN users AS u
+                                            ON n.UserID = u.ID
+                                            WHERE u.Username = '{username}';", connection);
+            var reader = await query.ExecuteReaderAsync();
+
+            List<NoteModel> notes = new List<NoteModel>();
+            while (await reader.ReadAsync())
+            {
+                notes.Add(new NoteModel((int)reader[0], (DateTime)reader[1], (string)reader[2]));
+            }
+            await reader.CloseAsync();
+            await connection.CloseAsync();
+            HabitGroupModel m;
+            return null;
         }
     }
 }
