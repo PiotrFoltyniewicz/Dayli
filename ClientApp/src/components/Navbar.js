f46c6e5a4e60601b8575e26a04844b3ed6a8ce4a
Mateusz Kaźmierczak <matteusz.kazm@gmail.com>
2023-10-24 14:51:21 +0200

    some style fixes, navbar and login TAB accessibility

--- ClientApp/src/components/Navbar.js
+++ ClientApp/src/components/Navbar.js
@@ -1,63 +1,71 @@
-import React, {useRef} from "react"
-import betterDayLogo from "../images/betterDayLogo.png"
+import React, { useRef } from 'react';
+import betterDayLogo from '../images/betterDayLogo.png';
 import { useAuth } from '../contexts/AuthContext';
 
 function Navbar(props) {
     const { token } = useAuth();
     const navbarRef = useRef();
+    const loginButtonRef = useRef();
     const showNavbar = () => {
-        navbarRef.current.classList.toggle("visible")
-    }
+        navbarRef.current.classList.toggle('visible');
+    };
+
     const handleLogInClick = () => {
-        props.setLogPopUp(prev => !prev) 
-    }
+        props.setLogPopUp((prev) => !prev);
+    };
+
+    const handleLoginEnter = (event) => {
+        if (event.key === 'Enter') {
+            loginButtonRef.current.click();
+        }
+    };
+
     return (
         <header className="header">
             <div className="navbar--top">
                 <a className="navbar--name" href="/">
-                    <img src={betterDayLogo} alt="Logo"></img>
+                    <img src={betterDayLogo} alt="Logo" />
                     BetterDay
                 </a>
-                <button
-                    className="navbar--control"
-                    onClick={showNavbar}
-                >
-                    <span></span><span></span><span></span>
+                <button className="navbar--control" onClick={showNavbar}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 </button>
             </div>
             <nav className="navbar" ref={navbarRef}>
-                <ul className="navbar--right" >
+                <ul className="navbar--right">
                     <li>
-                        <a
-                            className="navbar--right--element"
-                            href="/home">Home
+                        <a className="navbar--right--element" href="/home">
+                            Home
                         </a>
                     </li>
                     <li>
-                        <a
-                            className="navbar--right--element"
-                            href="/about">About
+                        <a className="navbar--right--element" href="/about">
+                            About
                         </a>
                     </li>
                     <li>
-                        {token ?
-                            <a
-                                className="navbar--right--element"
-                                href="/profile"
-                            >
+                        {token ? (
+                            <a className="navbar--right--element" href="/profile">
                                 Profile
                             </a>
-                            :
+                        ) : (
                             <span
-
+                                tabIndex="0"
                                 className="navbar--right--element"
-                                onClick={handleLogInClick}>
+                                onClick={handleLogInClick}
+                                onKeyPress={handleLoginEnter}
+                                ref={loginButtonRef}
+                            >
                                 Log in
-                            </span>}
+                            </span>
+                        )}
                     </li>
                 </ul>
             </nav>
         </header>
     );
 }
+
 export default Navbar;
