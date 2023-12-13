033abf3db397c5ad9ac6c13e7667c19423b5c803
Piotr Foltyniewicz <piotr.foltyniewicz@gmail.com>
2023-12-13 15:03:53 +0100

    Added image gallery

--- ClientApp/src/components/GuestHome.js
+++ ClientApp/src/components/GuestHome.js
@@ -1,6 +1,21 @@
 ﻿import quotePhoto from '../images/marcusAurelius.png'
+import ImageGallery from './ImageGallery';
 
 function GuestHome() {
+    const testImages = [
+        {
+            text: 'Plan your life',
+            url: '/images/testImg.jpg'
+        },
+        {
+            text: 'Take action',
+            url: '/images/testImg1.jpg'
+        },
+        {
+            text: 'Analyze yourself',
+            url: '/images/testImg2.jpg'
+        }
+    ];
 
     return (
         <div className='home--content' >
@@ -10,12 +25,7 @@ function GuestHome() {
             </div>
             <h1>Boost your productivity with Dayli</h1>
             <button>Join for free!</button>
-            <h2>Plan your life</h2>
-            <img src='.' width='200px'/>
-            <h2>Take action</h2>
-            <img src='.' width='200px'/>
-            <h2>Analyze yourself</h2>
-            <img src='.' width='200px' />
+            <ImageGallery images={ testImages } />
             {/* Dawaj tu ten login popup jak sie kliknie */}
             
         </div>
