b801880f9c5bac0474a3214c9b42b57b3d1f7857
Piotr Foltyniewicz <piotr.foltyniewicz@gmail.com>
2023-12-14 14:41:31 +0100

    Added another error

--- ClientApp/src/components/LoggedHome.js
+++ ClientApp/src/components/LoggedHome.js
@@ -1,26 +1,35 @@
-﻿import { useEffect } from "react";
+﻿import { useEffect, useState } from "react";
 import { useAuth } from '../contexts/AuthContext'
 
 function LoggedHome() {
 
     const { token } = useAuth();
-    let todayTasks = []
+    const [todayTasks, setTodayTasks] = useState([1]);
+    const [todayHabits, setTodayHabits] = useState([]);
+    const [todayNote, setTodayNote] = useState([]);
 
     useEffect(() => {
-        todayTasks = GetTodayTasks();
-    }, [])
+        async function GetTodayTasks() {
+            const response = await fetch('/api/Task/all', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    // JAK WYSŁAĆ TEN TOKEN??!!!?! >:(
+                    'Authorization': 'Bearer ' + token
+                },
+            });
+            const data = await response.json();
+            const output = data.map(x => (
+                <label className='task'>
+                    <input type='checkbox' checked={x.status} />
+                    {x.title}
+                </label>))
+            setTodayTasks(output);
+        }
+        GetTodayTasks();
+    }, []);
 
-    async function GetTodayTasks() {
-        const response = await fetch('/api/Task/all', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token
-            },
-        });
-        const data = await response.json();
-        return data;
-    }
+    
     return (
         <div className='home--content' >
             { /* Jakieś brane nazwy uzytkownika */}
@@ -28,7 +37,7 @@ function LoggedHome() {
             <div className='home--userContent'>
                 <div className='home--userContent--tasks'>
                     <h2>Today tasks</h2>
-                    { todayTasks }
+                    { todayTasks.length > 0 ? todayTasks : null}
                 </div>
                 <div className='home--userContent--habits'>
                     <h2>Habit tracker</h2>
