5a19f1bcb627f8a97c2f412676ff48f6bdd0201a
Piotr Foltyniewicz <piotr.foltyniewicz@gmail.com>
2024-02-16 21:08:52 +0100

    yES

--- ClientApp/src/components/HabitPage.js
+++ ClientApp/src/components/HabitPage.js
@@ -41,8 +41,22 @@ function HabitPage() {
         });
 
         if (habitListResponse.ok) {
+            // FILTR HABITLISTU I DODAĆ DO STATE
             const habitListData = await habitListResponse.json();
-            setHabitList(habitListData);
+            for (let name of habitList) {
+                const habitsToRow = []
+                for (let habitGroup of habits) {
+                    for (let habit of habitGroup.habits) {
+                        if (habit !== null && habit.title === name.title) {
+                            habitsToRow.push(habit);
+                        }
+                    }
+                }
+                if (habitsToRow.length > 0) {
+                    
+                }
+
+            }
         }
     }
 
@@ -169,6 +183,8 @@ function HabitPage() {
                 <h2>{`Today is ${chosenDate.getDate()} ${convertToMonthName(chosenDate.getMonth())} ${chosenDate.getFullYear()}`}</h2>
             </header>
             <main className='habitPage--main'>
+                <Calendar
+                    className='taskPage--calendar'/>
                 <section className='habitPage--main--habitList'>
                     <h3>{`Habits for ${convertToMonthName(chosenDate.getMonth())} ${chosenDate.getFullYear()}`}</h3>
                     <table className='habitPage--main--habitTable'>
